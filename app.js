bda3b25d4ea5068d999387078ff1d2fa47e97e01
alexandra <alexandradietz3@gmail.com>
2021-04-23 16:15:20 -0500

    add file with exercise

---
+++ app.js
@@ -0,0 +1,11 @@
+// ES5 Global Constants
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const PI = 3.14
+
+
+// 1. Let can not be redeclared like Var can.
+// 2. Const can not be redeclared or reassigned like Var can.
+// 3. Let can be reassigned unlike Const.
+// 4. Hoisting is calling something before it has been declared and initialized. 
